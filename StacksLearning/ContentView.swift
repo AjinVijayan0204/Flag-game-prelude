50370fde5baca33346ff3343eca23822640e1f5b
AjinVijayan0204 <ajinvijayan0204@gmail.com>
2022-10-12 20:03:38 +0530

    Initial Commit

---
+++ StacksLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StacksLearning
+//
+//  Created by Ajin on 12/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StacksLearning/StacksLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksLearningApp.swift
+//  StacksLearning
+//
+//  Created by Ajin on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct StacksLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
